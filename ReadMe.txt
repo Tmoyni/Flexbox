5b1fcfca4b9c8460cf1340df90afa17ba5d82551
triona <trionamoynihan@gmail.com>
2021-08-03 14:07:10 -0400

    updated some styling, added descriptions, fixed cross axis and main axis labels

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+[ ]  Flexbox Documentation in Confluence
+[ ]  How Flexbox Works
+[ ]  Flexbox Application MVP - simple boxes
+[ ]  Flexbox use in Dreamie
+[ ]  Flexbox Application Phase 2 - advanced layout
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    let topAxisText = $(".top-axis")
+    let sideAxisText = $(".side-axis")
     $(":button").on( "click", function() {
         $(this).parent().children("button").removeClass("selected")
         $(this).addClass("selected")
@@ -6,10 +8,19 @@ $(document).ready(function() {
         //identify property & value and update CSS
         let property = $(this).parent()[0].id;
         let value = $(this)[0].outerText;
-        $(".view-container").css(property, value)
+        $(".view-container-items").css(property, value)
+
+        // Updates Axis labels
+        if ($(".view-container-items")[0].style.cssText.includes("row")) {
+            topAxisText[0].innerText = "Main Axis"
+            sideAxisText[0].innerText = "Cross Axis"
+        } else {
+            topAxisText[0].innerText = "Cross Axis"
+            sideAxisText[0].innerText = "Main Axis"
+        }
 
         //add style text to "CSS" section
-        let css = $(".view-container")[0].attributes.style.textContent
+        let css = $(".view-container-items")[0].attributes.style.textContent
         $(".css-text")[0].innerText = css
     })
 });
